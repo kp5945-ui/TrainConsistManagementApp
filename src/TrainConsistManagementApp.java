46e368ccd96ab2cb829769c87e4371dc01c8a9b1
kp5945-ui <kushalpadala27@gmail.com>
2026-04-17 10:34:49 +0530

    UC18: Linear Search for Bogie ID

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,1 +1,18 @@
 public class TrainConsistManagementApp {
+
+    public static boolean searchBogie(String[] bogies, String key) {
+        for (String b : bogies) {
+            if (b.equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        String[] bogies = {"BG101","BG205","BG309","BG412","BG550"};
+
+        System.out.println(searchBogie(bogies, "BG309"));
+        System.out.println(searchBogie(bogies, "BG999"));
+    }
+}
--- src/TrainConsistManagementAppTest.java
+++ src/TrainConsistManagementAppTest.java
@@ -1,5 +1,35 @@
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class TrainConsistManagementAppTest {
+public class TrainConsistManagementAppTest {
 
+    @Test
+    void testSearch_BogieFound() {
+        String[] input = {"BG101","BG205","BG309","BG412","BG550"};
+        assertTrue(TrainConsistManagementApp.searchBogie(input, "BG309"));
+    }
+
+    @Test
+    void testSearch_BogieNotFound() {
+        String[] input = {"BG101","BG205","BG309","BG412","BG550"};
+        assertFalse(TrainConsistManagementApp.searchBogie(input, "BG999"));
+    }
+
+    @Test
+    void testSearch_FirstElementMatch() {
+        String[] input = {"BG101","BG205","BG309","BG412","BG550"};
+        assertTrue(TrainConsistManagementApp.searchBogie(input, "BG101"));
+    }
+
+    @Test
+    void testSearch_LastElementMatch() {
+        String[] input = {"BG101","BG205","BG309","BG412","BG550"};
+        assertTrue(TrainConsistManagementApp.searchBogie(input, "BG550"));
+    }
+
+    @Test
+    void testSearch_SingleElementArray() {
+        String[] input = {"BG101"};
+        assertTrue(TrainConsistManagementApp.searchBogie(input, "BG101"));
+    }
 }
